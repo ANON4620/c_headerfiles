7fecf524905f4379cde5337b218ccc65776f84f6
ANON4620 <swarnadeep.click@gmail.com>
2021-09-06 21:41:51 +0530

    FILE: infinp.h - Removed temporary file storage, Implemented reallocation of memory instead.

--- infinp.h
+++ infinp.h
@@ -6,6 +6,9 @@
 #ifndef _STDLIB_H
 #include <stdlib.h>
 #endif
+#ifndef _STDDEF_H
+#include <stddef.h>
+#endif
 #ifndef _STDBOOL_H
 #include <stdbool.h>
 #endif
@@ -15,37 +18,21 @@
 
 typedef char* String;
 
-void strinp(String* str, const bool allocated)
+void strinp(String* str)
 {
-	FILE *fp = NULL;
 	char ch;
 	int len = 1;
 
-	fp = fopen("temp.tmp", "w+");
-	chkfstatus(fp, 8, "temp.tmp");
 	while(1)
 	{
 		ch = fgetc(stdin);
 		if(ch == '\n')
 			break;
-		fputc(ch, fp);
+		*str = (String) realloc(*str, len * sizeof(
+char));
+		chkmem(*str);
+		*(*str + (len - 1)) = ch;
 		len++;
 	}
-	
-	// allocating heap memory
-	if(!allocated)
-		*str = (String) malloc(len * sizeof(char));
-	else
-		*str = (String) realloc(*str, len * sizeof(char));
-	chkmem(*str);
-	
-	// set pointer to the beginning of the file
-	fseek(fp, 0, SEEK_SET);
-
-	for(int i = 0; i < len - 1; i++)
-		*(*str + i) = fgetc(fp);
 	*(*str + (len - 1)) = '\0';
-	fclose(fp);
-
-	remove("temp.tmp");
 }
