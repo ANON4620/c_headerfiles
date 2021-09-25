9662b9b5f8e7263a18379ff3f60ae517ba692561
ANON4620 <swarnadeep.click@gmail com>
2021-09-25 11:47:17 +0530

    status.h: Added chkmem(), infinp.h: renamed to readline.h, Removed other files.

---
+++ readline.h
@@ -0,0 +1,38 @@
+#define _READLINE_H
+
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#endif
+#ifndef _STDDEF_H
+#include <stddef.h>
+#endif
+#ifndef _STDBOOL_H
+#include <stdbool.h>
+#endif
+#ifndef _STATUS_H
+#include "status.h"
+#endif
+
+typedef char* String;
+
+void readline(String* str)
+{
+	char ch;
+	int len = 1;
+
+	while(1)
+	{
+		ch = fgetc(stdin);
+		if(ch == '\n')
+			break;
+		*str = (String) realloc(*str, len * sizeof(
+char));
+		chkmem(*str);
+		*(*str + (len - 1)) = ch;
+		len++;
+	}
+	*(*str + (len - 1)) = '\0';
+}
