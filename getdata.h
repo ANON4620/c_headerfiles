ac736bb2f9dcbce3729faed549ffd98515f0e1b3
ANON4620 <swarnadeep.click@gmail com>
2022-01-17 23:51:03 +0530

    getdata.h : Bug fixes

--- getdata.h
+++ getdata.h
@@ -3,9 +3,12 @@ int getint(void)
     int n, s = 0;
     while((n = getchar()) != '\n')
     {
-        n = n - '0';
-        s = s * 10 + n;
+        if(n >= '0' && n <= '9')
+        {
+            n = n - '0';
+            s = s * 10 + n;
+        }
     }
-    
+
     return(s);
 }
